dfd8ac6305e831da05de68faec09b9fb2a8f4b4f
ZhijieJin <36421589+ZhijieJin@users.noreply.github.com>
2018-02-13 13:34:12 -0600

    Add files via upload

---
+++ ECE120/lab6/lab6_lib/hdl/Vending_Machine_NAND_struct_config.vhd
@@ -0,0 +1,11 @@
+-- Generation properties:
+--   Format              : hierarchical
+--   Generic mappings    : exclude
+--   Leaf-level entities : direct binding
+--   Regular libraries   : use library name
+--   View name           : include
+--   
+CONFIGURATION Vending_Machine_NAND_struct_config OF Vending_Machine_NAND IS
+   FOR struct
+   END FOR;
+END Vending_Machine_NAND_struct_config;
